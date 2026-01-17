5fb51d3dda1411eef2e09b2139be657f321a7260
Akash Sharma <akash.sharma@igniteconnex.com>
2026-01-17 23:15:19 +0530

    Imported all the db credentials from .env file

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,10 @@
+require("dotenv").config();
 const mysql = require("mysql2");
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: "signup_app",
+  database: process.env.DB_NAME,
 });
 
 db.connect((err) => {
