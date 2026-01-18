a128eb43bbc372e2ed916429648a4df0315f657f
Akash Sharma <akash.sharma@igniteconnex.com>
2026-01-18 23:35:25 +0530

    Integrated Login components into the main App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,11 @@
 import Signup from "./components/SignupForm";
+import Login from "./components/LoginForm";
 function App() {
   return (
     <div>
       <Signup />
+      <hr />
+      <Login />
     </div>
   );
 }
