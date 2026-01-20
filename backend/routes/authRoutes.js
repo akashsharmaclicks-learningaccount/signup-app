8c1f8ac56f878d6674bc74cc83ff366ba6f4541a
Akash Sharma <akash.sharma@igniteconnex.com>
2026-01-20 18:26:09 +0530

    Added a proctected route for testing purpose.

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const { signup, login } = require("../controllers/authController");
+const authMiddleware = require("../middleware/authMiddleware");
 router.post("/signup", signup);
 router.post("/login", login);
-
+router.get("/protected", authMiddleware, (req, res) => {
+  res.json({
+    message: "You accessed a protected route",
+    user: req.user,
+  });
+});
 module.exports = router;
